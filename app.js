65fa7519270f2f1b02a9fd6527f1d4117e541eb3
PalleGregersJensen <palle.gregers.jensen@gmail.com>
2023-05-19 15:15:29 +0200

    To spørgsmål til quizgame fungerer

--- app.js
+++ app.js
@@ -24,12 +24,54 @@ function answerQuestionOne() {
     const kathmandu = document.querySelector("#kathmandu");
     const phomPenh = document.querySelector("#phom-penh");
 
-  if (ulanBator.checked) {
-    document.querySelector("#question-one-dialog-result").textContent = "Korrekt";
-  } else if (phomPenh.checked) {
-    document.querySelector("#question-one-dialog-result").textContent = "Desværre forkert";
-  } else if (kathmandu.checked) {
-    document.querySelector("#question-one-dialog-result").textContent = "Desværre forkert";
+    if (ulanBator.checked) {
+        document.querySelector("#question-one-dialog-result").textContent = "Korrekt";
+    } else if (phomPenh.checked) {
+        document.querySelector("#question-one-dialog-result").textContent = "Desværre forkert";
+    } else if (kathmandu.checked) {
+        document.querySelector("#question-one-dialog-result").textContent = "Desværre forkert";
     }
-    console.log("Læses dette?")
+    console.log("Læses dette?");
+
+    const setTimeoutAtQuestions = setTimeout(closeDialogQuestionsOne, 2000);
 }
+
+function closeDialogQuestionsOne() {
+document.querySelector("#question-one-dialog").close();
+    questionTwo();
+    console.log("close dialog")
+}
+
+function questionTwo() {
+   console.log("question two");
+   document.querySelector("#question-two-dialog").showModal();
+   document
+     .querySelector("#question-two-dialog-answer-button")
+        .addEventListener("click", answerButtonClickedInQuestionTwo);
+    console.log("Spørgsmål 2")
+}
+
+function answerButtonClickedInQuestionTwo() {
+  answerQuestionTwo();
+}
+
+function answerQuestionTwo() {
+  const seanBean = document.querySelector("#sean-bean");
+  const gabrielByrne = document.querySelector("#gabriel-byrne");
+  const alanRickman = document.querySelector("#alan-rickman");
+
+  if (seanBean.checked) {
+    document.querySelector("#question-two-dialog-result").textContent = "Korrekt";
+  } else if (gabrielByrne.checked) {
+    document.querySelector("#question-two-dialog-result").textContent = "Desværre forkert";
+  } else if (alanRickman.checked) {
+    document.querySelector("#question-two-dialog-result").textContent = "Desværre forkert";
+  }
+  console.log("Læses dette i spørgsmål to?");
+  const timeOutToNextQuestion = setTimeout(closeDialogQuestionsTwo, 2000);
+}
+
+function closeDialogQuestionsTwo() {
+  document.querySelector("#question-two-dialog").close();
+}
+
