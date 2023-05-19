c5335465bde17f6e80432db80f8558b2592fd22c
PalleGregersJensen <palle.gregers.jensen@gmail.com>
2023-05-19 21:39:45 +0200

    Point/antal rigtige svar tilføjet

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 
 window.addEventListener("load", start);
 
+let points = 0;
+
 // Start app
 function start() {
   console.log("JS kører");
@@ -11,10 +13,10 @@ function start() {
 // Spørgsmål 1
 function questionOne() {
   console.log("button clicked");
-    document.querySelector("#question-one-dialog").showModal();
-    document
-      .querySelector("#question-one-dialog-answer-button")
-      .addEventListener("click", answerButtonClickedInQuestionOne);
+  document.querySelector("#question-one-dialog").showModal();
+  document
+    .querySelector("#question-one-dialog-answer-button")
+    .addEventListener("click", answerButtonClickedInQuestionOne);
 }
 
 function answerButtonClickedInQuestionOne() {
@@ -22,35 +24,37 @@ function answerButtonClickedInQuestionOne() {
 }
 
 function answerQuestionOne() {
-    const ulanBator = document.querySelector("#ulan-bator");
-    const kathmandu = document.querySelector("#kathmandu");
-    const phomPenh = document.querySelector("#phom-penh");
+  const ulanBator = document.querySelector("#ulan-bator");
+  const kathmandu = document.querySelector("#kathmandu");
+  const phomPenh = document.querySelector("#phom-penh");
 
-    if (ulanBator.checked) {
-        document.querySelector("#question-one-dialog-result").textContent = "Korrekt";
-    } else if (phomPenh.checked) {
-        document.querySelector("#question-one-dialog-result").textContent = "Desværre forkert";
-    } else if (kathmandu.checked) {
-        document.querySelector("#question-one-dialog-result").textContent = "Desværre forkert";
-    }
-    console.log("Læses dette?");
+  if (ulanBator.checked) {
+    document.querySelector("#question-one-dialog-result").textContent = "Korrekt";
+    points +=1;
+    document.querySelector("#points").textContent = points;
+  } else if (phomPenh.checked) {
+    document.querySelector("#question-one-dialog-result").textContent = "Desværre forkert";
+  } else if (kathmandu.checked) {
+    document.querySelector("#question-one-dialog-result").textContent = "Desværre forkert";
+  }
+  console.log("Læses dette?");
 
-    const setTimeoutAtQuestions = setTimeout(closeDialogQuestionsOne, 2000);
+  const setTimeoutAtQuestions = setTimeout(closeDialogQuestionOne, 2000);
 }
 // Spørgsmål 1 dialogvindue lukkes
-function closeDialogQuestionsOne() {
-document.querySelector("#question-one-dialog").close();
-    questionTwo();
-    console.log("close dialog")
+function closeDialogQuestionOne() {
+  document.querySelector("#question-one-dialog").close();
+  questionTwo();
+  console.log("close dialog");
 }
 
 // Spørgsmål 2
 function questionTwo() {
-   console.log("question two");
-   document.querySelector("#question-two-dialog").showModal();
-   document
-     .querySelector("#question-two-dialog-answer-button")
-        .addEventListener("click", answerButtonClickedInQuestionTwo);
+  console.log("question two");
+  document.querySelector("#question-two-dialog").showModal();
+  document
+    .querySelector("#question-two-dialog-answer-button")
+    .addEventListener("click", answerButtonClickedInQuestionTwo);
   console.log("Spørgsmål 2");
 }
 
@@ -65,17 +69,19 @@ function answerQuestionTwo() {
 
   if (seanBean.checked) {
     document.querySelector("#question-two-dialog-result").textContent = "Korrekt";
+    points +=1;
+    document.querySelector("#points").textContent = points;
   } else if (gabrielByrne.checked) {
     document.querySelector("#question-two-dialog-result").textContent = "Desværre forkert";
   } else if (alanRickman.checked) {
     document.querySelector("#question-two-dialog-result").textContent = "Desværre forkert";
   }
   console.log("Læses dette i spørgsmål to?");
-  const timeOutToNextQuestion = setTimeout(closeDialogQuestionsTwo, 2000);
+  const timeOutToNextQuestion = setTimeout(closeDialogQuestionTwo, 2000);
 }
 
 // Spørgsmål 2 dialog lukkes
-function closeDialogQuestionsTwo() {
+function closeDialogQuestionTwo() {
   document.querySelector("#question-two-dialog").close();
   questionThree();
 }
@@ -91,26 +97,66 @@ function questionThree() {
 }
 
 function answerButtonClickedInQuestionThree() {
-  answerQuestionThree()
-};
+  answerQuestionThree();
+}
 
 function answerQuestionThree() {
- const everyThirdYear = document.querySelector("#EP3");
- const everyFourthYear = document.querySelector("#EP4");
- const everyFifthYear = document.querySelector("#EP5");
+  const everyThirdYear = document.querySelector("#EP3");
+  const everyFourthYear = document.querySelector("#EP4");
+  const everyFifthYear = document.querySelector("#EP5");
 
- if (everyThirdYear.checked) {
-   document.querySelector("#question-three-dialog-result").textContent = "Desværre forkert" ;
+  if (everyThirdYear.checked) {
+    document.querySelector("#question-three-dialog-result").textContent = "Desværre forkert";
   } else if (everyFourthYear.checked) {
-   document.querySelector("#question-three-dialog-result").textContent = "Desværre forkert";
- } else if (everyFifthYear.checked) {
-   document.querySelector("#question-three-dialog-result").textContent = "Korrekt";
- }
- console.log("Læses dette i spørgsmål tre?");
- const timeOutToNextQuestion = setTimeout(closeDialogQuestionsThree, 2000); 
+    document.querySelector("#question-three-dialog-result").textContent = "Desværre forkert";
+  } else if (everyFifthYear.checked) {
+    document.querySelector("#question-three-dialog-result").textContent = "Korrekt";
+    points +=1;
+    document.querySelector("#points").textContent = points;
+  }
+  console.log("Læses dette i spørgsmål tre?");
+  const timeOutToNextQuestion = setTimeout(closeDialogQuestionThree, 2000);
 }
 
 // Spørgsmål 3 dialog lukkes
-function closeDialogQuestionsThree() {
+function closeDialogQuestionThree() {
+  document.querySelector("#question-three-dialog").close();
+  questionFour();
+}
+
+// Spørgsmål 4
+function questionFour() {
+  console.log("question four");
+  document.querySelector("#question-four-dialog").showModal();
+  document
+    .querySelector("#question-four-dialog-answer-button")
+    .addEventListener("click", answerButtonClickedInQuestionFour);
+  console.log("Spørgsmål 4");
+}
+
+function answerButtonClickedInQuestionFour() {
+  answerQuestionFour();
+}
+
+function answerQuestionFour() {
+  const everyThirdYear = document.querySelector("#EP3");
+  const everyFourthYear = document.querySelector("#EP4");
+  const everyFifthYear = document.querySelector("#EP5");
+
+  if (everyThirdYear.checked) {
+    document.querySelector("#question-three-dialog-result").textContent = "Desværre forkert";
+  } else if (everyFourthYear.checked) {
+    document.querySelector("#question-three-dialog-result").textContent = "Desværre forkert";
+  } else if (everyFifthYear.checked) {
+    document.querySelector("#question-three-dialog-result").textContent = "Korrekt";
+    points +=1;
+    document.querySelector("#points").textContent = points;
+  }
+  console.log("Læses dette i spørgsmål tre?");
+  const timeOutToNextQuestion = setTimeout(closeDialogQuestionThree, 2000);
+}
+
+// Spørgsmål 4 dialog lukkes
+function closeDialogQuestionThree() {
   document.querySelector("#question-three-dialog").close();
 }
